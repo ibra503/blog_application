d6fd9471e2ee7dda0167763b7df3b2e4924c893b
Ibra <aouzalibrahim80@gmail.com>
2025-09-05 20:13:24 +0100

    adding post comments model and users

--- blogs/blogs/wsgi.py
+++ blogs/blogs/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogs.settings')
 
 application = get_wsgi_application()
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("posts/", views.PostListView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+]
